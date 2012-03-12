5e841c5c4a48f38698dbc3a10290c4fed132d5d7
Your Name <jfriesne@gmail.com>
2012-03-12 00:04:36 -0700

    Added icons

--- gen/com/sugoi/electricsign/R.java
+++ gen/com/sugoi/electricsign/R.java
@@ -11,7 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int pin=0x7f020000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
